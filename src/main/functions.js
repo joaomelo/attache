545674bf1f05580ee8caf2c80ca63ed353f35ffe
joão melo <j@melo.plus>
2021-01-25 22:21:42 -0300

    feature: logging with new log module

--- src/main/functions.js
+++ src/main/functions.js
@@ -1,4 +1,5 @@
 import { initDb } from '../app/db';
+import { createLogger } from '../app/log';
 import { get } from '../app/request';
 import { createScaleSerpSearch } from '../app/search';
 import { saveFreshSnapshotsForStakes } from '../domain/snapshots';
@@ -9,10 +10,13 @@ export async function fireSaveFreshSnapshotsForStakes () {
   const db = await initDb('firebase');
   if (!db) throw new Error('Unable to connect to firestore');
 
+  const logger = createLogger(db);
+
   const key = process.env.SCALE_SERP_KEY;
   if (!key) throw new Error('SCALE SERP api key not found as environment variable');
   const search = createScaleSerpSearch({ get, key });
 
   await saveFreshSnapshotsForStakes(db, search);
-  log('snapshot finished');
+
+  logger.info('snapshot finished');
 }
