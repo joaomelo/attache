64e3e0301c25655764c95e7aec3a646707093880
joão melo <j@melo.plus>
2021-03-01 20:04:04 -0300

    feat: moved email to inline styling

--- src/app/view/render-track.js
+++ src/app/view/render-track.js
@@ -1,21 +1,23 @@
 import { sortByField } from '../../helpers';
-import { resetCss, typography, colors, sizing, position, effects } from './styles';
+import { style, baseCss, text, position, colors, sizes, effects } from './styles';
 import { letterIcon, firstIcon, pageIcon } from './icons';
 
 export function renderTrackReport (track) {
   const message = `
     <html>
-      <head>
-        ${resetCss}
-        ${typography}
-        ${colors}
-        ${sizing}
-        ${position}
-        ${effects}
-      </head>
+      <head>${baseCss}</head>
       <body>
-        <div class="p3 bg-nt-900">
-          <main class="wm1 mauto p2 bg-nt-050 rounded">
+        <div ${style(
+          colors.bg.p9,
+          sizes.padding.p3
+        )}>
+          <main ${style(
+            sizes.width.max1,
+            sizes.margin.auto,
+            sizes.padding.p2,
+            colors.bg.n1,
+            effects.rounded
+          )}>
             ${renderTitle()}
             ${renderJoin(track.terms, renderTerm)}
             ${renderFooter(track.stake.id)}
@@ -32,26 +34,52 @@ export function renderTrackReport (track) {
 
 function renderTitle () {
   return `
-    <h1 class="heavy cl-nt-900 fs4 mt1 mb2 text-center">Track Report</h1>
+    <h1 ${style(
+      text.weight.heavy,
+      text.size.s4,
+      text.align.center,
+      colors.cl.n9,
+      sizes.margin.mt1,
+      sizes.margin.mb2
+    )}>
+      Track Report
+    </h1>
     ${renderLine()}   
   `;
 }
 
 function renderTerm (term) {
   return `
-    <section class="mt2 ml1 mr1 p1">
-      <span class="flex align-center cl-pr-900">
+    <div ${style(
+      sizes.margin.mt2,
+      sizes.margin.ml1,
+      sizes.margin.mr1,
+      sizes.padding.p1
+    )}>
+      <span ${style(
+        position.flex,
+        position.align.center,
+        colors.cl.p9,
+        text.weight.heavy,
+        text.size.s3
+      )}>
         ${letterIcon()}
-        <h2 class="ml1 fs3">
-          <span class="normal cl-nt-400">Searching for</span>
-          <span class="heavy">${term.term}</span>
+        <h2>
+          <span ${style(
+            text.weight.normal,
+            sizes.margin.ml1,
+            colors.cl.n4
+          )}>
+            Searching for
+          </span>
+          ${term.term}
         </h2>
       </span>
-      <div class="p2">
+      <div ${style(sizes.padding.p2)}>
         ${renderTrend(term.trend)}
         ${renderJoin(term.rankings, renderRanking)}
       </div>
-    </section>
+    </div>
   `;
 }
 
@@ -59,11 +87,23 @@ function renderTrend (trend) {
   const sortedPosition = sortByField(trend, 'when', false);
 
   return `
-    <span class="mb1 flex align-center cl-pr-900">
+    <span ${style(
+      position.flex,
+      position.align.center,
+      sizes.margin.mb1,
+      colors.cl.p9,
+      text.size.s3,
+      text.weight.heavy
+    )}>
       ${firstIcon(24)}
-      <h3 class="fs3 normal">
-        <span class="ml1">First</span>
-        <span class="cl-nt-400">results were</span>
+      <h3 ${style(sizes.margin.ml1)}>
+        First
+        <span ${style(
+          colors.cl.n7,
+          text.weight.normal
+        )}>
+          results were
+        </span>
       </h3>
     </span>
     ${renderJoin(sortedPosition, renderTrendPosition)}
@@ -74,9 +114,16 @@ function renderTrendPosition (trendPosition) {
   const maxSize = 30;
   const prettyWhen = prettifyWhen(trendPosition.when);
   return `
-    <p class="normal cl-nt-900 fs2">
+    <p ${style(
+      text.weight.normal,
+      text.size.s2,
+      colors.cl.n9
+    )}>
       ${prettyWhen}: 
-      <a href="${trendPosition.page}" class="heavy">
+      <a 
+        ${style(text.weight.heavy)}
+        href="${trendPosition.page}"
+      >
         ${trendPosition.page.substr(0, maxSize)}${trendPosition.page.length > maxSize ? '...' : ''}
       </a>
     </p>
@@ -86,12 +133,32 @@ function renderTrendPosition (trendPosition) {
 function renderRanking (ranking) {
   const sortedPosition = sortByField(ranking.positions, 'when', false);
   return `
-    <span class="mt2 mb1 flex align-center cl-pr-900">
+    <span 
+      ${style(
+        position.flex,
+        position.align.center,
+        sizes.margin.mt2,
+        sizes.margin.mb1,
+        colors.cl.p9,
+        text.weight.heavy,
+        text.size.s3
+      )}>
       ${pageIcon(24)}
-      <h3 class="fs3 normal">
-        <span class="ml1 cl-nt-400">Page</span>
-        <span class="heavy">${ranking.page}</span>
-        <span class="cl-nt-400">ranked</span>
+      <h3>
+        <span ${style(
+          sizes.margin.ml1,
+          colors.cl.n7,
+          text.weight.normal
+        )}>
+          Page
+        </span>
+        ${ranking.page}
+        <span ${style(
+          colors.cl.n7,
+          text.weight.normal
+        )}>
+          ranked
+        </span>
       </h3>
     </span>
     ${renderJoin(sortedPosition, renderRankingPosition)}
@@ -102,8 +169,61 @@ function renderRankingPosition (rankingPosition) {
   const prettyWhen = prettifyWhen(rankingPosition.when);
   const prettyPosition = rankingPosition.position === 0 ? 'Not found' : `${rankingPosition.position}º`;
   return `
-    <p class="normal cl-nt-900 fs2">${prettyWhen}: <span class="heavy">${prettyPosition}</span></p>
+    <p ${style(
+      text.weight.normal,
+      text.size.s2,
+      colors.cl.n9
+    )}>
+      ${prettyWhen}: 
+      <span ${style(text.weight.heavy)}>
+        ${prettyPosition}
+      </span>
+    </p>
   `;
+}
+
+function renderFooter (stakeId) {
+  return `
+    ${renderLine()}
+    <div ${style(
+      sizes.padding.p1,
+      sizes.margin.mt2
+    )}>
+      <p ${style(
+        text.weight.normal,
+        text.size.s1,
+        colors.cl.n3
+      )}>
+        Track report for Stake ${stakeId}
+      </p>
+
+      <p ${style(
+        sizes.margin.mt1,
+        text.weight.normal,
+        text.size.s2,
+        colors.cl.n7
+      )}>
+        Made with 
+        <a 
+          ${style(text.underline)}
+          target="_blank" 
+          href="https://github.com/joaomelo/attache"
+        >
+          Attaché
+        </a>
+        . 
+        A open source project available at GitHub.
+      </p>
+    </div>
+  `;
+}
+
+function renderLine () {
+  return `<hr ${style(effects.line)}>`;
+}
+
+function renderJoin (array, render) {
+  return array.map(i => render(i)).join('');
 }
 
 function prettifyWhen (w) {
@@ -111,24 +231,4 @@ function prettifyWhen (w) {
   const m = (w.getMonth() + 1).toString().padStart(2, '0');
   const d = (w.getDate() + 1).toString().padStart(2, '0');
   return `${y}-${m}-${d}`;
-}
-
-function renderFooter (stakeId) {
-  return `
-    ${renderLine()}
-    <footer class="p1 mt2">
-      <p class="normal cl-nt-400 fs1">Track report for Stake ${stakeId}</p>
-      <p class="mt1 normal fs2 cl-nt-700">
-        Made with <a class="underline" target="_blank" href="https://github.com/joaomelo/attache">Attaché</a>. A open source project available at GitHub.
-      </p>
-    </footer>
-  `;
-}
-
-function renderLine () {
-  return '<hr style="border-top: 1px solid #BCCCDC">';
-}
-
-function renderJoin (array, render) {
-  return array.map(i => render(i)).join('');
 }
--- src/app/view/styles.js
+++ src/app/view/styles.js
@@ -1,8 +1,9 @@
-export const resetCss = `
+export const baseCss = `
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet">
   <style>
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, pre, a, img, small, 
-    strong, b, u, i, ol, ul, li, article, footer, header
-    {
+    strong, b, u, i, ol, ul, li, article, footer, header {
       margin: 0;
       margin-block-start: 0;
       margin-block-end: 0;
@@ -14,106 +15,99 @@ export const resetCss = `
       text-decoration: none;
       color: inherit;
     }
-    body { line-height: 1; }
-    ol, ul { list-style: none; }
+
+    html { 
+      font-family: 'Roboto', sans-serif; 
+    }
+
+    body { 
+      line-height: 1; 
+    }
   </style>
 `;
 
-export const typography = `
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet">
-  <style>
-    html { font-family: 'Roboto', sans-serif; }
-    .normal { font-weight: 400; }
-    .heavy { font-weight: 900; }
-    .text-center { text-align: center; }
-    .underline { text-decoration: underline; }
-    .fs1 { font-size: 10px; }
-    .fs2 { font-size: 16px; }
-    .fs3 { font-size: 20px; }
-    .fs4 { font-size: 26px; }  
-  </style>
-`;
+export const text = {
+  weight: {
+    normal: 'font-weight: 400;',
+    heavy: 'font-weight: 900;'
+  },
+  align: {
+    center: 'text-align: center;'
+  },
+  size: {
+    s1: 'font-size: 10px;',
+    s2: 'font-size: 16px;',
+    s3: 'font-size: 20px;',
+    s4: 'font-size: 26px;'
+  },
+  underline: 'text-decoration: underline;'
+};
 
-export const colors = `
-  <style>
-    .bg-pr-050 { background-color: #E0FCFF; }
-    .bg-pr-100 { background-color: #BEF8FD; }
-    .bg-pr-200 { background-color: #87EAF2; }
-    .bg-pr-300 { background-color: #54D1DB; }
-    .bg-pr-400 { background-color: #38BEC9; }
-    .bg-pr-500 { background-color: #2CB1BC; }
-    .bg-pr-600 { background-color: #14919B; }
-    .bg-pr-700 { background-color: #0E7C86; }
-    .bg-pr-800 { background-color: #0A6C74; }
-    .bg-pr-900 { background-color: #044E54; }
-    .bg-nt-050 { background-color: #F0F4F8; }
-    .bg-nt-100 { background-color: #D9E2EC; }
-    .bg-nt-200 { background-color: #BCCCDC; }
-    .bg-nt-300 { background-color: #9FB3C8; }
-    .bg-nt-400 { background-color: #829AB1; }
-    .bg-nt-500 { background-color: #627D98; }
-    .bg-nt-600 { background-color: #486581; }
-    .bg-nt-700 { background-color: #334E68; }
-    .bg-nt-800 { background-color: #243B53; }
-    .bg-nt-900 { background-color: #102A43; }
-    .cl-pr-050 { color: #E0FCFF; }
-    .cl-pr-100 { color: #BEF8FD; }
-    .cl-pr-200 { color: #87EAF2; }
-    .cl-pr-300 { color: #54D1DB; }
-    .cl-pr-400 { color: #38BEC9; }
-    .cl-pr-500 { color: #2CB1BC; }
-    .cl-pr-600 { color: #14919B; }
-    .cl-pr-700 { color: #0E7C86; }
-    .cl-pr-800 { color: #0A6C74; }
-    .cl-pr-900 { color: #044E54; }
-    .cl-nt-050 { color: #F0F4F8; }
-    .cl-nt-100 { color: #D9E2EC; }
-    .cl-nt-200 { color: #BCCCDC; }
-    .cl-nt-300 { color: #9FB3C8; }
-    .cl-nt-400 { color: #829AB1; }
-    .cl-nt-500 { color: #627D98; }
-    .cl-nt-600 { color: #486581; }
-    .cl-nt-700 { color: #334E68; }
-    .cl-nt-800 { color: #243B53; }
-    .cl-nt-900 { color: #102A43; }
-  </style>
-`;
+export const colors = {
+  bg: {
+    p1: 'background-color: #BEF8FD;',
+    p3: 'background-color: #54D1DB;',
+    p7: 'background-color: #0E7C86;',
+    p9: 'background-color: #044E54;',
+    n1: 'background-color: #D9E2EC;',
+    n3: 'background-color: #9FB3C8;',
+    n7: 'background-color: #334E68;',
+    n9: 'background-color: #102A43;'
+  },
+  cl: {
+    p1: 'color: #BEF8FD;',
+    p3: 'color: #54D1DB;',
+    p4: 'color: #38BEC9;',
+    p7: 'color: #0E7C86;',
+    p9: 'color: #044E54;',
+    n1: 'color: #D9E2EC;',
+    n3: 'color: #9FB3C8;',
+    n7: 'color: #334E68;',
+    n9: 'color: #102A43;'
+  }
+};
 
-export const sizing = `
-  <style>
-    .mauto { margin: auto; }
-    .mt1 { margin-top: 8px; }
-    .mt2 { margin-top: 16px; }
-    .mt3 { margin-top: 32px; }
-    .ml1 { margin-left: 8px; }
-    .ml2 { margin-left: 16px; }
-    .ml3 { margin-left: 32px; }
-    .mr1 { margin-right: 8px; }
-    .mr2 { margin-right: 16px; }
-    .mr3 { margin-right: 32px; }
-    .mb1 { margin-bottom: 8px; }
-    .mb2 { margin-bottom: 16px; }
-    .mb3 { margin-bottom: 32px; }
-    .p1 { padding: 8px; }
-    .p2 { padding: 16px; }
-    .p3 { padding: 32px; }
-    .pl1 { padding-left: 8px; }
-    .pl2 { padding-left: 16px; }
-    .pl3 { padding-left: 32px; }
-    .wm1 { max-width: 576px; }
-  </style>
-`;
+export const sizes = {
+  padding: {
+    p1: 'padding: 8px;',
+    p2: 'padding: 16px;',
+    p3: 'padding: 32px;',
+    pl1: 'padding-left: 8px;',
+    pl2: 'padding-left: 16px;',
+    pl3: 'padding-left: 32px;'
+  },
+  margin: {
+    auto: 'margin: auto;',
+    mt1: 'margin-top: 8px;',
+    mt2: 'margin-top: 16px;',
+    mt3: 'margin-top: 32px;',
+    ml1: 'margin-left: 8px;',
+    ml2: 'margin-left: 16px;',
+    ml3: 'margin-left: 32px;',
+    mr1: 'margin-right: 8px;',
+    mr2: 'margin-right: 16px;',
+    mr3: 'margin-right: 32px;',
+    mb1: 'margin-bottom: 8px;',
+    mb2: 'margin-bottom: 16px;',
+    mb3: 'margin-bottom: 32px;'
+  },
+  width: {
+    max1: 'max-width: 576px;'
+  }
+};
 
-export const position = `
-  <style>
-    .flex { display: flex; }
-    .align-center { align-items: center; }
-  </style>
-`;
+export const position = {
+  flex: 'display: flex;',
+  align: {
+    center: 'align-items: center;'
+  }
+};
 
-export const effects = `
-  <style>
-    .rounded { border-radius: 8px; }
-  </style>
-`;
+export const effects = {
+  rounded: 'border-radius: 8px;',
+  line: 'border-top: 1px solid #BCCCDC;'
+};
+
+export function style (...rest) {
+  return `style="${rest.join('')}"`;
+}
