765626dc2e6b4e8d1248be055eafa61a07697333
joão melo <j@melo.plus>
2021-03-06 20:42:36 -0300

    refactor: removed unused code

--- src/app/request/axios.js
+++ src/app/request/axios.js
@@ -4,16 +4,6 @@ const instance = axios.create({
   validateStatus: null
 });
 
-export async function status (url) {
-  try {
-    const response = await instance.get(url);
-    return response.status;
-  } catch (error) {
-    const badGatewayStatusCode = 502;
-    return badGatewayStatusCode;
-  }
-};
-
 export async function get (url, params) {
   const response = await instance.get(url, { params });
   return response.data;
