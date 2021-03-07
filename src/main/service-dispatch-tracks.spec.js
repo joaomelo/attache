6b5710f8008243597f2c67b611b21f369de70091
joão melo <j@melo.plus>
2021-03-07 14:10:36 -0300

    test: improved integration test to actually track emails

--- src/main/service-dispatch-tracks.spec.js
+++ src/main/service-dispatch-tracks.spec.js
@@ -1,9 +1,18 @@
 import firebase from 'firebase-functions-test';
+import { stakes, snapshots } from '../../tests/fixtures';
+import { createStakesCollection } from '../domain/stakes';
+import { createSnapshotsCollection } from '../domain/snapshots';
 import { testIfIntegration } from '../helpers';
 import { tracksScheduledService } from './services';
 
+jest.mock('../domain/stakes/collection');
+jest.mock('../domain/snapshots/collection');
+
 describe('dispatch tracks reports firebase function', () => {
   testIfIntegration()('sanity run just to check firebase function integrity', async () => {
+    createStakesCollection.mockReturnValue({ queryAll: () => stakes });
+    createSnapshotsCollection.mockReturnValue({ querySuccessfulLastDays: () => snapshots.filter(s => s.success) });
+
     const test = firebase({
       projectId: process.env.GCLOUD_PROJECT
     });
@@ -11,6 +20,6 @@ describe('dispatch tracks reports firebase function', () => {
     const tracksService = test.wrap(tracksScheduledService);
     const result = await tracksService();
 
-    expect(result).toBe(0);
+    expect(result).toBe(3);
   });
 });
--- tests/fixtures/stakes.js
+++ tests/fixtures/stakes.js
@@ -1,14 +1,16 @@
+const baseInbox = process.env.DEFAULT_FROM_EMAIL.replace('@', '.');
+
 export const stakes = [
   {
     id: '9164167b-6588-4dc1-a710-19a43a836df5',
     pages: ['cloud.google', 'firebase.google'],
     terms: ['cloud', 'serverless'],
-    emails: ['some@email.com', 'another@email.com']
+    emails: [`${baseInbox}.a@maildrop.cc`, `${baseInbox}.b@maildrop.cc`]
   },
   {
     id: 'efcd94d9-cfe9-47bd-b128-57c03ca12a96',
     pages: ['aws.amazon.com'],
     terms: ['cloud'],
-    emails: ['my@email.com']
+    emails: [`${baseInbox}.c@maildrop.cc`]
   }
 ];
